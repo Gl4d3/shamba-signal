4b91f89b5fee02634b02dbae78fd306207d6584f
Anthony Kioko <108263849+Gl4d3@users.noreply.github.com>
2026-08-04 11:36:28 +0300

    feat: expose isolated TabFM adapter

---
+++ experiments/tabfm/src/shamba_tabfm/__init__.py
@@ -0,0 +1,5 @@
+"""Isolated TabFM adapter for Shamba Signal."""
+
+from shamba_tabfm.provider import TabFMPredictionProvider, load_pytorch_provider
+
+__all__ = ["TabFMPredictionProvider", "load_pytorch_provider"]
